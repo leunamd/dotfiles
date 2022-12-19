28937136319c10daaabf78c0235a6de53b99c245
manuel <>
2022-12-19 21:01:33 +0100

    updated spotify trayer script to only show metadata when a song is playing

--- dot_config/xmonad/scripts/executable_spotify.sh
+++ dot_config/xmonad/scripts/executable_spotify.sh
@@ -1,6 +1,9 @@
 running=$(pidof spotify)
 if [ "$running" != "" ]; then
-    artist=$(playerctl metadata artist --player=spotify)
-    song=$(playerctl metadata title --player=spotify| cut -c 1-60)
-    echo -n "$artist - $song"
+    status=$(playerctl status --player=spotify)
+    if [ "$status" == "Playing" ]; then
+      artist=$(playerctl metadata artist --player=spotify)
+      song=$(playerctl metadata title --player=spotify| cut -c 1-60)
+      echo -n "$artist - $song"
+    fi
 fi
