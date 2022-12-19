1e736b1c607cc00d2e3283e4ff25944d49ba772b
manuel <>
2022-12-19 20:30:17 +0100

    changed spotify script to only work for spotify pid

--- dot_config/xmonad/scripts/executable_spotify.sh
+++ dot_config/xmonad/scripts/executable_spotify.sh
@@ -1,6 +1,6 @@
 running=$(pidof spotify)
 if [ "$running" != "" ]; then
-    artist=$(playerctl metadata artist)
-    song=$(playerctl metadata title | cut -c 1-60)
+    artist=$(playerctl metadata artist --player=spotify)
+    song=$(playerctl metadata title --player=spotify| cut -c 1-60)
     echo -n "$artist - $song"
 fi
