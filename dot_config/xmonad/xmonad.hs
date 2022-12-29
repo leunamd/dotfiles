5c5235f258886a3779ce024c8ede3d9ecbcfbf44
manuel <>
2022-12-29 18:37:41 +0100

    updated startup discord parameters

--- dot_config/xmonad/xmonad.hs
+++ dot_config/xmonad/xmonad.hs
@@ -346,7 +346,7 @@ myStartupHook = do
   spawnOnce "frame"
   spawn "~/.config/xmonad/scripts/systray.sh"
   --foreground applications
-  spawnOnce "discord --disable-smooth-scrolling"                                       --from ~/bin
+  spawnOnce "discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling --disable-smooth-scrolling"
   spawnOnce "telegram-desktop"
   spawnOnce "spotify"
   setWMName "LG3D"
