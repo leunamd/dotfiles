963faaeca985c868bb673c44a0c43e3f266e49a3
manuel <>
2023-02-15 12:00:08 +0100

    added geoclue2 for redshift location

--- dot_config/xmonad/xmonad.hs
+++ dot_config/xmonad/xmonad.hs
@@ -338,6 +338,7 @@ myXmobarPP h =
 myStartupHook = do
   spawnOnce "xrandr --output DP-4 --mode 1920x1080 --rate 144 --primary --right-of HDMI-0 --auto"
   --background applications
+  spawnOnce "/usr/lib/geoclue-2.0/demos/agent"
   spawnOnce "lxsession"
   spawnOnce "numlockx"
   spawnOnce "~/.config/xmonad/scripts/volumeicon.sh"
